feb675373643163f78a82732d596563fb121cd4d
Bernardo Baldiviezo <fataldesign616@gmail.com>
2016-03-11 12:28:18 +0000

    Started building motion function
    Right very early stage. All 4 profile functions can be executed from a
    single iteration, working on it!

--- js/receiver.js
+++ js/receiver.js
@@ -15,7 +15,7 @@ window.onload = function() {
     var screenSaver = $('#slides_container');
 
     //Variable to establish which profile to load (MAIN function)
-    var profiletoLoad;
+    var profiletoLoad = loadProfile_1; //Load first profile at the start
 
     video.each(function() {
         var vidID = $(this).attr('id');
@@ -26,79 +26,69 @@ window.onload = function() {
     });
 
     //Slideshow
-    $.fn.rotator = function(settings) {
-        settings = jQuery.extend({
-            interval: 5000,
-            speed: 1500
-        }, settings);
+    var idleTime = 0;
+    var idleInterval = setInterval(timerIncrement, 5000); // 2.5 seconds
 
-        return this.each(function() {
-            var $t = $(this),
-                $item = $t.children().addClass('item')
+    var $t = $('.slides'),
+        $item = $t.children().addClass('item')
 
-            $t.addClass('rotator');
+        $t.addClass('rotator');
 
-            if ($item.length > 1) {
-                $item.first().addClass('current')
-                //console.log($('.current').attr('class'));
-                var currentProfile = $('.current').attr('class').split(' ');
-                setInterval(function() {
-                    var c = $t.find('.current');
-                    if (c.next().length === 0) {
-                        c.removeClass('current')
-                        $item.first().addClass('current')
-                    } else {
-                        c.removeClass('current').next().addClass('current')
-                    };                
-                    //console.log($('.current').attr('class'));
-                    currentProfile = $('.current').attr('class').split(' ');
-                    
-                    if(currentProfile[0] == 'Profile_1'){
-                        console.log('go to profile 1');
-                        profiletoLoad = loadProfile_1
-                    } else if (currentProfile[0] == 'Profile_2'){
-                        console.log('go to profile 2');
-                        profiletoLoad = loadProfile_2
-                    } else if (currentProfile[0] == 'Profile_3'){
-                        console.log('go to profile 3');
-                        profiletoLoad = loadProfile_4
-                    } else if (currentProfile[0] == 'Profile_4'){
-                        console.log('go to profile 4');
-                        profiletoLoad = loadProfile_4
-                    }
-                    
-                }, settings.interval);
-            }
-        });
-    };
+    if ($item.length > 1) {
+        $item.first().addClass('current')
+        //console.log($('.current').attr('class'));
+    }
 
+    var currentProfile = $('.current').attr('class').split(' ');
 
-    //TEST Log screensaver current time to screen
-    function onTrackedVideoFrame(currentTime) {
-        $(".current").text(currentTime);
-        //console.log(currentTime);
-    };
+    function timerIncrement() {
+        var c = $t.find('.current');
+        if (c.next().length === 0) {
+            c.removeClass('current')
+            $item.first().addClass('current')
+        } else {
+            c.removeClass('current').next().addClass('current')
+        };
+        //console.log($('.current').attr('class'));
+        currentProfile = $('.current').attr('class').split(' ');
+
+        if (currentProfile[0] == 'Profile_1') {
+            console.log('go to profile 1');
+            profiletoLoad = loadProfile_1
+        } else if (currentProfile[0] == 'Profile_2') {
+            console.log('go to profile 2');
+            profiletoLoad = loadProfile_2
+        } else if (currentProfile[0] == 'Profile_3') {
+            console.log('go to profile 3');
+            profiletoLoad = loadProfile_4
+        } else if (currentProfile[0] == 'Profile_4') {
+            console.log('go to profile 4');
+            profiletoLoad = loadProfile_4
+        }
+    }
+
 
     //MAIN load Profile functions
     function loadProfile_1() {
-        alert('load profile 1');
+        //alert('load profile 1');
     }
 
     function loadProfile_2() {
-        alert('load profile 2');
+        //alert('load profile 2');
+        $('#Container').addClass('Profile_2')
+        screenSaver.css('opacity', '0');
+        screenSaver.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+            $('#video_container #Profile_2').get(0).play();
+        });
     }
-    
+
     function loadProfile_3() {
-        alert('load profile 2');
+        //alert('load profile 2');
     }
-    
+
     function loadProfile_4() {
-        alert('load profile 2');
+        //alert('load profile 2');
     }
-
-
-    //Start slideshow
-    $(".slides").rotator();
 
 
     //MOTION DETECTION EVENTS START HERE
@@ -110,10 +100,14 @@ window.onload = function() {
             //Fire motion detection events here
 
             //TODO when users steps in, play the 'intro' function entirely, then set a time out of 30 seconds, if nothing else happens. Return to Screensaver
+            
+            //Clear timer in slideshow so it stops on selected profile 
+            //clearInterval(idleInterval);
+            
             //profiletoLoad();
 
-            //console.log(profiletoLoad);
-            
+            console.log(currentProfile[0]);
+
         } else {
             console.log('Pi is still')
             //document.body.style.backgroundColor = "black";
