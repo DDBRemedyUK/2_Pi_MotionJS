afb1674c8feaa40606ee26d2903881aa31c1ef70
Bernardo Baldiviezo <fataldesign616@gmail.com>
2016-03-16 16:00:25 +0000

    Reset function coded in
    Videos go back to start when the function resets, need to debug the
    waiting time before function resets @_@

--- js/receiver.js
+++ js/receiver.js
@@ -26,7 +26,7 @@ window.onload = function() {
     });
 
     //Slideshow
-    var idleTime = 0;
+    var idleTime = 1;
     var idleInterval = setInterval(timerIncrement, 5000); // 5 seconds
 
     var $t = $('.slides'),
@@ -42,6 +42,10 @@ window.onload = function() {
     var currentProfile = $('.current').attr('class').split(' ');
 
     function timerIncrement() {
+
+        idleTime = idleTime + 1;
+        console.log(idleTime);
+
         var c = $t.find('.current');
         if (c.next().length === 0) {
             c.removeClass('current')
@@ -65,10 +69,11 @@ window.onload = function() {
 
     //NOTE: creating global variable to be used as a closure to avoid the event firing more than once.
     var profileLoaded;
+
     function loadProfile() {
         if (!profileLoaded) {
             profileLoaded = true;
-            
+
             //Add profile class to overall Container
             $('#Container').addClass(currentProfile[0]);
             //Fade screensaver
@@ -76,163 +81,236 @@ window.onload = function() {
             //Fade in looping BG
             $('#BG_source video').attr('src', 'video/' + currentProfile[0] + '_BG.mp4');
             $('#BG_source').addClass('faded_in');
-             
-            //dynamicNav();
-            
-            //Once faded out, play profile introduction video
-            var currentVideo = $('#video_container' + ' ' + '#'+currentProfile[0]).get(0)
 
-            screenSaver.on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+            //dynamicNav();
+
+            //Once faded out, play profile introduction video
+            var currentVideo = $('#video_container' + ' ' + '#' + currentProfile[0]).get(0)
+
+            screenSaver.fadeIn('slow', function() {
                 currentVideo.play();
             });
             //Stop video at 5 seconds (tap to continue screen)
             var checkedFirst = false;
             var checkedSecond = false;
 
-            currentVideo.addEventListener('timeupdate', function(event)
-                                          {
+            currentVideo.addEventListener('timeupdate', function(event) {
                 //console.log(currentVideo.currentTime); 
-                if (currentVideo.currentTime >= 5 && !checkedFirst)
-                {
+                if (currentVideo.currentTime >= 5 && !checkedFirst) {
                     checkedFirst = true;
                     currentVideo.pause();
                 }
 
-                if (currentVideo.currentTime >= 13 && !checkedSecond)
-                {
+                if (currentVideo.currentTime >= 13 && !checkedSecond) {
                     checkedSecond = true;
                     currentVideo.pause();
                     $('#video_container').addClass('invisible');
-                    $('#video_container').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
+                    $('#video_container').fadeIn('slow', function() {
                         console.log('video end');
+                        currentVideo.currentTime = 0;//Bring video back to beggining
                         $('#Bio').addClass('faded_in');
                     });
-                    
-                    //Make Bio smaller, fade rest of the elements after 3 seconds
-                    $('#Bio').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function(){
-                        console.log('Bio just faded in!');
-                        //setTimeout(function(){
-                            /*$('#video_container').delay(3000).addClass('hidden');
-                            
-                            $('#Container').addClass('Phase_2');
-                            
-                            $('#Bio').addClass('Reduced');*/
 
-                            //$('#Testimonial' + '.' + currentProfile[0]).addClass('faded_in');
-                            //$('#Keypoints' + '.' + currentProfile[0]).addClass('faded_in');
-                            
-                            
-                        //}, 3000);
-                        
-                        
-                        $('#video_container').delay(3000).fadeOut('slow').queue(function(){
+                    //Make Bio smaller, fade rest of the elements after 3 seconds
+                    $('#Bio').fadeIn('slow', function() {
+                        console.log('Bio container animation completed');
+                        $('#video_container').delay(3000).fadeOut('slow').queue(function() {
+                            //$('#video_container').addClass('unclickable');
                             $('#Container').addClass('Phase_2');
                             $('#Bio').addClass('Reduced');
                         });
-
-                        //$('#Container').delay(3000).addClass('Phase_2');
-
-                        //$('#Bio').delay(3000).addClass('Reduced');
-                        
                     });
-                    
+
                 }
             });
 
 
-            $('#video_container').click(function(event){
+            $('#video_container').click(function(event) {
                 currentVideo.play();
                 console.log('replay');
+                $('#video_container').addClass('unclickable');
             });
-            //NOTE: Main function could continue here, however it will be moved to the bottom as the stand might not be triggered by the motion sensor.
         }
+        
+        //Run Warning modal window
+         warningModal();
     }
 
     //Modal windows function
-    function popupModals(){
+    function popupModals() {
         var popupBtn = $('#Keypoints ul li button');
         var otherModals = $('#Keypoints ul li div');
         var popupClass
-        
-        popupBtn.each(function(){
-            $(this).on('click', function(){
+
+        popupBtn.each(function() {
+            $(this).on('click', function() {
                 console.log($(this).attr('class'));
                 otherModals.fadeOut('slow');
                 $(this).next('div').fadeIn('slow');
+                clearTimer();
             });
         });
 
         //close btn function
-        $('.close_btn').on('click', function(){
+        $('.close_btn').on('click', function() {
             $(this).parent('div').fadeOut('slow');
+            clearTimer();
         });
     };
-    
+
     //Patient navigation function
-    function patientSwitch(){
+    function patientSwitch() {
         var patientBtn = $('#Patients ul li');
-        
-        patientBtn.each(function(){
-            $(this).on('click', function(){
+
+        patientBtn.each(function() {
+            $(this).on('click', function() {
                 var switchProfile = $(this).attr('class')
                 console.log(switchProfile);
-                
+
                 /*$('#Container').removeClass(currentProfile[0]);
                 $('#Container').removeClass('Phase_2');*/
                 //Reset container class
                 $('#Container').removeClass('Phase_2');
                 $('#Container').attr('class', '');
-                
+
                 $('#Container').addClass(switchProfile);
                 $('#Container').addClass('Phase_2');
-                
+
                 /*$('#Testimonial, #Keypoints').each(function(){
                     $(this).fadeOut('slow');
                     $('#Testimonial' + '.' + switchProfile).fadeIn('slow');
                 });*/
-                
-                console.log('#Patients ul li' + '.' +switchProfile);
-                
-                $('#Patients ul li' + '.' +switchProfile).insertAfter('#Patients ul li:last')
-                
+
+                console.log('#Patients ul li' + '.' + switchProfile);
+
+                $('#Patients ul li' + '.' + switchProfile).insertAfter('#Patients ul li:last')
+
                 $('#BG_source video').attr('src', 'video/' + switchProfile + '_BG.mp4');
-                
-   
+
+                clearTimer();
+
             });
         });
-        
-        
+
+
     };
-    
+
     //Patient navigation reacts dynamically to overall container class
-    function dynamicNav(){
-        $('#Patients ul li').each(function(){
-            if($(this).hasClass(currentProfile[0])){
+    function dynamicNav() {
+        $('#Patients ul li').each(function() {
+            if ($(this).hasClass(currentProfile[0])) {
                 console.log('The container class matches with one of the Nav items');
                 $(this).insertAfter('#Patients ul li:last');
             }
         });
     }
 
+    //Warning timer when user has been idle
+    var counter
+    var countDown
+    var modalInterval
+    var modalIncrement
+    function warningModal() {
+        //Check if user has been inactive
+        var modalInterval = setTimeout(modalIncrement, 5000); //Max idle time: 10 seconds
+        
+        function modalIncrement(){
+            console.log('5 secs have passed');
+            $('#IdleModal').fadeIn('slow', function(){
+                counter = 10;
+                countDown = setInterval(modalTimer, 1000); //1000 will  run it every 1 second
+                function modalTimer() {
+                    counter = counter - 1;
+                    if (counter <= 0) {
+                        //counter ended, do something here
+                        return;
+                    }
+                    $('.countdown').text(counter);
+                    //Do code for showing the number of seconds here;
+                    if (counter == 1) {
+                        //console.log('timer has been completed');
+                        revertEverything();
+                        //counter = 20;
+                        clearInterval(countDown);
+                    }
+                }
+                $('#IdleModal .close_btn').on('click', function(){
+                    resetModalTimer();
+                });
+            });
+        }
+        
+    };
     
+    
+    function resetModalTimer(){
+        console.log('reset the countdown interval here');
+        clearInterval(countDown);
+        $('.countdown').text('20')
+        modalInterval = setTimeout(modalIncrement, 5000); //Reset max idle time here
+    }
+
+
+    //Revert function to reset to home screens
+    function revertEverything() {
+
+        console.log('Modal timeout completed');
+        
+        //$('#IdleModal').attr('style', '');
+        //$('#IdleModal').hide();
+        //clearInterval(countDown);
+        //$('.countdown').text('20');
+        //var countDown = setInterval(timer, 1000);
+        
+        $('#IdleModal').fadeOut('slow');
+        
+        profileLoaded = false;
+        
+        //run the revert functions here
+        var idleTime = 1;
+        idleInterval = setInterval(timerIncrement, 5000);
+        
+        $('video.Profile')[0].pause();
+        $('video.Profile')[0].currentTime = 0;
+        
+        $('#Container').attr('class', '');
+        $('#Container').removeClass('Phase_2');
+        $('#slides_container').attr('style', '');
+
+        $('#video_container').attr('class', '');
+        $('#video_container').attr('style', '');
+        
+        $('#BG_source').attr('class', '');
+
+        $('#Bio').attr('class', '');
+    }
+
+
+    function clearTimer() {
+        clearInterval(idleInterval);
+        idleTime = 0;
+        console.log(idleTime);
+        console.log('timer cleared');
+    };
+
     //Calling our functions
     popupModals();
     patientSwitch();
 
-    
+
     //MOTION DETECTION EVENTS START HERE
     //FUTURE: Include a counter that gets reset if the counter goes to green (use banner counter as base)
     socket.on('pirstatus', function(data) {
         if (data) {
             //Fire motion detection event here
-            
+
             //Clear timer in slideshow so it stops on selected profile
-            
+
             //Only trigger the main function if the container section does NOT have any class
-            if($('#Container').attr('class') == ''){
+            if ($('#Container').attr('class') == '') {
                 console.log('Pi says motion was detected')
-                
-                clearInterval(idleInterval);
+
+                clearTimer();
 
                 loadProfile();
 
@@ -251,14 +329,15 @@ window.onload = function() {
     });
 
     //Backup Screen tap function
-    if($('#Container').attr('class') == ''){
-        $('#Container').on('click', function(){
+    if ($('#Container').attr('class') == '') {
+        $('#Container').one('click', function() {
             console.log('User tapped screen')
-            clearInterval(idleInterval);
+            clearTimer();
             loadProfile();
             console.log(currentProfile[0]);
         });
     }
-    
-    
+
+
+
 }
